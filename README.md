57f1717cdb578b38af8b5b013c6110fa5f956a8b
Kyungwon Chun <ruddyscent@gmail.com>
2021-09-28 15:33:12 +0900

    Write ticker, transaction, orderbook on MongoDB.
    * Use threads for the better responsibility of keyboard input, i.e.
    ctrl+c.
    * Write ticker, transaction, orderbook on each time series collection.
    * Use 'currency_pair' as metadata.
    * Typecast of numeric data to numerical types.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cuddly-potato
-Collect financial data
+Collect cryptocurrency trade data from Korbit, Inc.
+
+This program was tested on NVIDIA Xavier-NX.
+
+# Requirements
+mongodb >= 5.0
+
+The following packages and corresponding dependencies from the jetpack repository.
+* python3-pyqt5.qtwebsockets
+* python3-pymongo
--- pyqt.py
+++ pyqt.py
@@ -4,70 +4,120 @@ import datetime
 import json
 import sys
 
-import PyQt5.QtWidgets as qtw
-import PyQt5.QtCore as qtc
-import PyQt5.QtWebSockets as qtws
+from pymongo import MongoClient
 
-class KorbitWsClient(qtc.QObject):
-    received = qtc.pyqtSignal(dict)
-    # closed = qtc.pyqtSignal()
+from PyQt5.QtCore import QObject, QCoreApplication, QUrl, QThread, pyqtSignal, pyqtSlot
+from PyQt5.QtWebSockets import QWebSocket
+
+class KorbitWsClient(QObject):
+    received = pyqtSignal(dict)
 
     def __init__(self):
         super().__init__()
         
-        self.websocket = qtws.QWebSocket()
+        self.websocket = QWebSocket()
         self.websocket.connected.connect(self.onConnected)
-        # self.websocket.disconnected.connect(self.closed)
-        # self.websocket.textMessageReceived.connect(self.onTextMessageReceived)
-        self.websocket.textFrameReceived.connect(self.onTextMessageReceived)
-        uri = "wss://ws.korbit.co.kr/v1/user/push"
-        self.websocket.open(qtc.QUrl(uri))
+        self.websocket.textMessageReceived.connect(self.onTextMessageReceived)
+        uri = 'wss://ws.korbit.co.kr/v1/user/push'
+        self.websocket.open(QUrl(uri))
 
-    @qtc.pyqtSlot()
+    @pyqtSlot()
     def onConnected(self):
-        now = datetime.datetime.now()
+        kst = datetime.timezone(datetime.timedelta(hours=9))
+        now = datetime.datetime.now(kst)
         timestamp = int(now.timestamp() * 1000)
 
         subscribe_fmt = {
-            "accessToken": None,
-            "timestamp": timestamp,
-            "event": "korbit:subscribe",
-            "data": {
-                "channels": ["ticker:btc_krw"]
+            'accessToken': None,
+            'timestamp': timestamp,
+            'event': 'korbit:subscribe',
+            'data': {
+                'channels': [
+                    'ticker',
+                    'orderbook',
+                    'transaction',
+                ]
             }
         }
         subscribe_data = json.dumps(subscribe_fmt)
         self.websocket.sendTextMessage(subscribe_data)
         
-    @qtc.pyqtSlot(str)
+    @pyqtSlot(str)
     def onTextMessageReceived(self, msg: str):
         data = json.loads(msg)
         self.received.emit(data)
 
 
-class MainObject(qtc.QObject):
+class MainObject(QObject):
     def __init__(self):
         super().__init__()
 
         self.ws = KorbitWsClient()
-        # self.producer_thread = qtc.QThread()
-        # self.ws.moveToThread(self.producer_thread)
+        self.producer_thread = QThread()
+        self.ws.moveToThread(self.producer_thread)
         self.ws.received.connect(self.print_data)
-        # self.producer_thread.start()
+        self.producer_thread.start()
 
-    @qtc.pyqtSlot(dict)
-    def print_data(self, data):
-        timestamp = data.get('timestamp')
-        data_dict = data.get('data')
-        last = data_dict.get('last')
-        now = datetime.datetime.fromtimestamp(int(timestamp) / 1000)
+        client = MongoClient()
+        self.db = client.korbit
         
+        cols = self.db.list_collection_names()
+        for col in ['ticker', 'transaction', 'orderbook']:
+            if col in cols:
+                continue
 
-if __name__ == "__main__":
+            self.db.create_collection(
+                col, 
+                timeseries = {
+                    'timeField': 'timestamp',
+                    'metaField': 'currency_pair',
+                    'granularity': 'seconds'
+                    }
+                )
+
+    @pyqtSlot(dict)
+    def print_data(self, data):
+        data_dict = data['data']
+        event = data['event']
+        
+        if event in ['korbit:push-ticker', 'korbit:push-transaction', 'korbit:push-orderbook']:
+            del data_dict['channel']
+            
+            timestamp = int(data_dict['timestamp'])
+            kst = datetime.timezone(datetime.timedelta(hours=9))
+            data_dict['timestamp'] = datetime.datetime.fromtimestamp(int(timestamp) / 1000, kst)
+            
+        if event == 'korbit:push-ticker':
+            numeric_field = ['ask', 'bid', 'change', 'high', 'last', 'low', 'open', 'volume']
+            for field in numeric_field:
+                data_dict[field] = float(data_dict[field])
+            self.db.ticker.insert_one(data_dict)
+
+        elif event == 'korbit:push-transaction':
+            numeric_field = ['amount', 'price']
+            for field in numeric_field:
+                data_dict[field] = float(data_dict[field])
+            self.db.transaction.insert_one(data_dict)
+
+        elif event == 'korbit:push-orderbook':
+            for ask in data_dict['asks']:
+                ask['amount'] = float(ask['amount'])
+                ask['price'] = float(ask['price'])
+                
+            for bid in data_dict['bids']:
+                bid['amount'] = float(bid['amount'])
+                bid['price'] = float(bid['price'])
+
+            self.db.orderbook.insert_one(data_dict)
+
+        else:
+            pass
+
+    
+if __name__ == '__main__':
     import signal
 
-    # signal.signal(signal.SIGINT, sigint_handler)
-    app = qtc.QCoreApplication(sys.argv)
+    app = QCoreApplication(sys.argv)
     signal.signal(signal.SIGINT, lambda *x: app.quit())
     main_object = MainObject()
     sys.exit(app.exec())
